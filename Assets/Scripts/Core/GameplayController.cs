0bd66d2c57ada57a7d2a8ce3fc2f5184dec427fe
Vladyslav Yefimenko <118617711+Slain505@users.noreply.github.com>
2023-12-11 02:22:29 +0100

    Add Options

--- Assets/Scripts/Core/GameplayController.cs
+++ Assets/Scripts/Core/GameplayController.cs
@@ -1,4 +1,5 @@
 ﻿using Player;
+using UI;
 using UnityEngine;
 
 namespace Core
@@ -7,7 +8,12 @@ namespace Core
     {
         [SerializeField] private PlayerConfig playerConfig;
         [SerializeField] private GameObject playerPrefab;
+        [SerializeField] private GameObject enemyPrefab; 
         [SerializeField] private Transform playerSpawnPoint;
+        [SerializeField] private BoxCollider2D policeSpawnArea;
+        [SerializeField] private LayerMask policeSpawnAreaLayerMask;
+        [SerializeField] private int attemptsToSpawnPolice = 10;
+        [SerializeField] private float spawnRate;
         public static GameplayController Instance { get; private set; }
         private Player.Player player;
 
@@ -26,16 +32,70 @@ namespace Core
             player.Setup(playerConfig);
         }
         
-        //todo: implement Police
         private void SpawnEnemy()
         {
-            Debug.Log("Spawn enemy called.");
+            if (Level.Level.Instance.IsGamePaused)
+            {
+                return; 
+            }
             
+            Debug.Log("Spawn enemy called.");
+            var spawned = false;
+
+            for (int i = 0; i < attemptsToSpawnPolice; i++)
+            {
+                var spawnPosition = GetRandomPositionInSpawnArea();
+
+                if (!IsPositionOccupied(spawnPosition))
+                {
+                    Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+                    spawned = true;
+                    break;
+                }
+            }
+
+            if (!spawned)
+            {
+                Debug.Log("Failed to find an unoccupied position for police spawn after " + attemptsToSpawnPolice + " attempts.");
+            }
         }
-        
+
+        private bool IsPositionOccupied(Vector3 spawnPosition)
+        {
+            var size = new Vector2(1.2000000f, 1.20000005f);
+            var angle = 0f;
+
+            Collider2D[] collidersInArea = Physics2D.OverlapCapsuleAll(spawnPosition, size, CapsuleDirection2D.Vertical, angle, policeSpawnAreaLayerMask);
+
+            foreach (var collider in collidersInArea)
+            {
+                if (collider.gameObject.CompareTag("Police"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
         private void Start()
         {
             SpawnPlayer();
+            Countdown.Instance.OnCountdownFinished += GameStartState;
+        }
+
+        private void GameStartState()
+        {
+            InvokeRepeating("SpawnEnemy", spawnRate + 4f, spawnRate);
+        }
+
+        Vector2 GetRandomPositionInSpawnArea()
+        {
+            Bounds bounds = policeSpawnArea.bounds;
+            var x = Random.Range(bounds.min.x, bounds.max.x);
+            var y = Random.Range(bounds.min.y, bounds.max.y);
+
+            return new Vector2(x, y);
         }
     }
 }
--- Assets/Scripts/UI/Popups.cs
+++ Assets/Scripts/UI/Popups.cs
@@ -8,12 +8,16 @@ namespace UI
     {
         [SerializeField] private GameObject _endGamePopup;
         [SerializeField] private GameObject _pausePopup;
+        [SerializeField] private GameObject _optionPopup;
         [SerializeField] private Button _pauseButton;
         [SerializeField] private Button _resumeButton;
         [SerializeField] private Button _restartButton;
+        [SerializeField] private Button _optionButton;
+        [SerializeField] private Button _optionCloseButton;
         [SerializeField] private Button _mainMenuButton;
         [SerializeField] private Button _restartGameOverButton;
         [SerializeField] private Button _mainMenuGameOverButton;
+        [SerializeField] private Button _resetPlayerPrefsButton;
         [SerializeField] private Text _yourScoreText;
         [SerializeField] private Text _highScoreText;
     
@@ -25,25 +29,47 @@ namespace UI
             _pauseButton.onClick.AddListener(OnPauseButtonClicked);
             _resumeButton.onClick.AddListener(OnResumeButtonClicked);
             _restartButton.onClick.AddListener(OnRestartButtonClicked);
+            _optionButton.onClick.AddListener(OnOptionsButtonClicked);
+            _optionCloseButton.onClick.AddListener(OnOptionsCloseButtonClicked);
             _mainMenuButton.onClick.AddListener(OnMainMenuButtonClicked);
             _restartGameOverButton.onClick.AddListener(OnRestartButtonClicked);
             _mainMenuGameOverButton.onClick.AddListener(OnMainMenuButtonClicked);
+            _resetPlayerPrefsButton.onClick.AddListener(ResetPlayerPrefs);
         }
 
         private void OnMainMenuButtonClicked()
         {
+            Player.Player.Instance.UpdateCoinBalance();
             SceneManager.LoadScene(0);
         }
 
         private void OnRestartButtonClicked()
         {
+            Level.Level.Instance.Resumed();
             SceneManager.LoadScene(1);
         }
 
         private void OnPauseButtonClicked()
         {
-            _pausePopup.SetActive(true);
-            Level.Level.Instance.Paused();
+            if (_pausePopup.activeSelf)
+            {
+                _pausePopup.SetActive(false);
+                Level.Level.Instance.Resumed();
+            }
+            else if (_optionPopup.activeSelf)
+            {
+                _optionPopup.SetActive(false);
+                _pausePopup.SetActive(true);
+            }
+            else if (_endGamePopup.activeSelf)
+            {
+                _pauseButton.onClick.RemoveListener(OnPauseButtonClicked);
+            }
+            else
+            {
+                _pausePopup.SetActive(true);
+                Level.Level.Instance.Paused();
+            }
         }
 
         private void OnResumeButtonClicked()
@@ -51,12 +77,31 @@ namespace UI
             _pausePopup.SetActive(false);
             Level.Level.Instance.Resumed();
         }
-    
+
+        private void OnOptionsButtonClicked()
+        {
+            _optionPopup.SetActive(true);
+            _pausePopup.SetActive(false); 
+        }
+        
+        private void OnOptionsCloseButtonClicked()
+        {
+            _optionPopup.SetActive(false);
+            _pausePopup.SetActive(true);
+        }
+
         public void OnPlayerDied()
         {
             _endGamePopup.SetActive(true);
             _highScoreText.text = PlayerPrefs.GetInt("HighScore").ToString();
             _yourScoreText.text = PlayerPrefs.GetInt("Score").ToString();
+        }
+        
+        private void ResetPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.Save();
+            Debug.Log("PlayerPrefs reset.");
         }
     
         private void OnDestroy()
@@ -64,6 +109,7 @@ namespace UI
             _pauseButton.onClick.RemoveListener(OnPauseButtonClicked);
             _resumeButton.onClick.RemoveListener(OnResumeButtonClicked);
             _restartButton.onClick.RemoveListener(OnRestartButtonClicked);
+            _optionButton.onClick.RemoveListener(OnOptionsButtonClicked);
             _mainMenuButton.onClick.RemoveListener(OnMainMenuButtonClicked);
             _endGamePopup.SetActive(false);
         }
